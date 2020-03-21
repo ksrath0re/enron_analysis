2579aa157e8269116101c2cb3b58db56bee26b01
kuldeepsinghlive@gmail.com <kuldeepsinghlive@gmail.com>
2020-03-21 01:06:20 -0700

    added support for exception handling

--- process_enron_data.py
+++ process_enron_data.py
@@ -3,9 +3,9 @@ import numpy as np
 import pandas as pd
 import json
 
-emails_df = pd.read_csv('./data/emails.csv', nrows=100)
+emails_df = pd.read_csv('./data/emails.csv')
 print(emails_df.shape)
-print(emails_df.head())
+#print(emails_df.head())
 
 
 def get_content_from_email(msg):
@@ -43,20 +43,30 @@ print('---------------')
 
 sender_receiver = {}
 key_headers.insert(0, 'file')
+i = 0
 for row in emails_df.itertuples(index=False):
-    row = row._asdict()
-    row = dict(zip(key_headers, row.values()))
-    print(row)
-    sender = str(row['From'][0])
-    receivers = list(row['To'])
+    try:
+        row = row._asdict()
+        row = dict(zip(key_headers, row.values()))
+        sender = str(row['From'][0])
+        receivers = list(row['To'])
 
-    for receiver in receivers:
-        key = (sender, receiver.replace("\n", ""))
-        key = str(key)
-        if key not in sender_receiver.keys():
-            sender_receiver[key] = [row]
-        else:
-            sender_receiver[key].append(row)
+        for receiver in receivers:
+            key = (sender, receiver.replace("\n", ""))
+            key = str(key)
+            if key not in sender_receiver.keys():
+                sender_receiver[key] = [row]
+            else:
+                sender_receiver[key].append(row)
+        i += 1
+        if i % 50000 == 0:
+            print("Processed {} records! ".format(i))
+    except(RuntimeError, ValueError, TypeError):
+        # print("Error item is : ", items)
+        with open("error.txt", "a") as text_file, open("raw_files.txt", "a") as raw_file:
+            text_file.write("for line : {} , Error item is : {}\n".format(i, row))
+            raw_file.write("{}\n".format(str(row['file'])))
+    print("Processed {} records! ".format(i))
 
 with open('sender_receiver_pd.json', 'w') as fp:
     json.dump(sender_receiver, fp)
